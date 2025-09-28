11ba93eefc9d8e21db95b733c2c813cb19dd9fc5
Soni Maulana M <sonimaulanam@gmail.com>
2025-09-28 16:55:48 +0700

    update detail

--- app/Filament/Resources/LinkTerkaitResource.php
+++ app/Filament/Resources/LinkTerkaitResource.php
@@ -14,6 +14,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
