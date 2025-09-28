6d263549ab28ea6a60b3ccb12419fb4c6e96c269
Soni Maulana M <sonimaulanam@gmail.com>
2025-09-28 16:51:09 +0700

    update detail

--- resources/views/fe/pages/wisata.blade.php
+++ resources/views/fe/pages/wisata.blade.php
@@ -137,26 +137,6 @@
 
         </div>
 
-        <!-- Section Sponsor -->
-        <div class="py-10 mt-12 bg-white shadow rounded-2xl">
-            <div class="max-w-6xl mx-auto">
-                <div
-                    class="flex items-center gap-6 px-4 py-4 overflow-x-auto md:justify-center whitespace-nowrap scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent">
-                    @forelse ($sponsor as $item)
-                        <div class="flex justify-center flex-shrink-0">
-                            <img src="{{ asset('storage/' . $item->logo) }}" alt="{{ $item->nama_instansi ?? 'Sponsor' }}"
-                                class="object-contain h-24 mix-blend-multiply" />
-                        </div>
-                    @empty
-                        <div class="flex justify-center w-full">
-                            <p class="text-gray-500">Tidak ada data sponsor yang tersedia saat ini.</p>
-                        </div>
-                    @endforelse
-                </div>
-            </div>
-
-        </div>
-
         <!-- Dropdown Filter Wisata -->
         <div class="p-6 mt-8 bg-white border shadow border-slate-200 rounded-3xl">
             <div class="flex flex-col justify-between gap-3 md:flex-row">
@@ -170,6 +150,8 @@
                 <canvas id="chartDropdown" class="w-full "></canvas>
             </div>
         </div>
+
+
         <!-- Link Terkait -->
         <div class="mt-12 md:mt-28">
             <div class="max-w-2xl mx-auto">
@@ -218,7 +200,26 @@
             </div>
         </div>
 
-        
+        <!-- Section Sponsor -->
+        <div class="py-10 mt-12 bg-white shadow rounded-2xl">
+            <div class="max-w-6xl mx-auto">
+                <div
+                    class="flex items-center gap-6 px-4 py-4 overflow-x-auto md:justify-center whitespace-nowrap scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent">
+                    @forelse ($sponsor as $item)
+                        <div class="flex justify-center flex-shrink-0">
+                            <a href="{{ $item->link_web ?? '#' }}" target="_blank">
+                                <img src="{{ asset('storage/' . $item->logo) }}" alt="{{ $item->nama_instansi ?? 'Sponsor' }}" class="object-contain h-24 mix-blend-multiply" />
+                            </a>
+                        </div>
+                    @empty
+                        <div class="flex justify-center w-full">
+                            <p class="text-gray-500">Tidak ada data sponsor yang tersedia saat ini.</p>
+                        </div>
+                    @endforelse
+                </div>
+            </div>
+
+        </div>
     </main>
 @endsection
 
